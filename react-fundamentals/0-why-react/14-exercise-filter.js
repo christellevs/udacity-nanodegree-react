b2da3463109a8d0a11207800096c3a6ffdc71655
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-07-30 23:18:42 +0100

    react fundamentals exercises done

--- react-fundamentals/0-why-react/14-exercise-filter.js
+++ react-fundamentals/0-why-react/14-exercise-filter.js
@@ -1,14 +1,13 @@
-/* Using .map()
+/* Using .filter()
  *
- * Using the musicData array and .map():
- *   - return a string for each item in the array in the following format
- *     <album-name> by <artist> sold <sales> copies
- *   - store the returned data in a new albumSalesStrings variable
+ * Using the musicData array and .filter():
+ *   - return only album objects where the album's name is
+ *     10 characters long, 25 characters long, or anywhere in between
+ *   - store the returned data in a new `results` variable
  *
  * Note:
  *   - do not delete the musicData variable
  *   - do not alter any of the musicData content
- *   - do not format the sales number, leave it as a long string of digits
  */
 
 const musicData = [
@@ -17,16 +16,14 @@ const musicData = [
   { artist: 'Beyonce', name: 'Lemonade', sales: 1554000 },
   { artist: 'Chris Stapleton', name: 'Traveller', sales: 1085000 },
   { artist: 'Pentatonix', name: 'A Pentatonix Christmas', sales: 904000 },
-  {
-    artist: 'Original Broadway Cast Recording',
-    name: 'Hamilton: An American Musical', sales: 820000
-  },
+  { artist: 'Original Broadway Cast Recording', 
+    name: 'Hamilton: An American Musical', sales: 820000 },
   { artist: 'Twenty One Pilots', name: 'Blurryface', sales: 738000 },
   { artist: 'Prince', name: 'The Very Best of Prince', sales: 668000 },
   { artist: 'Rihanna', name: 'Anti', sales: 603000 },
   { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
 ];
 
-const albumSalesStrings = 'Replace this message with your code!';
+const results = musicData.filter((album) => album.name.length > 9 && album.name.length < 26);
 
-console.log(albumSalesStrings);
+console.log(results);
